53d95401e1b0c2ca7e7b99cdaed648e7be7251b0
Filip Bořil <filip.boril@sinnerschrader.com>
2020-04-21 23:14:04 +0200

    Updated footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Container from "react-bootstrap/Container";
 
-const footer = (props) => {
+const footer = () => {
   return (
       <footer className="pb-3">
           <hr />
           <Container>
-              <span className="text-muted">Filip Bořil 2019 ©</span>
+              <span className="text-muted">Filip Bořil 2019 – 2020 ©</span>
           </Container>
       </footer>
   );
